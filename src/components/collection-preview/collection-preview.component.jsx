0437d70027cd78f5f22efff8c7380b0a147bf9c2
Jack <60209373+OpoJack@users.noreply.github.com>
2022-01-02 17:09:28 -0500

    Added header component
    Added header components/styles

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import CollectionItem from "../collection-item/collection-item.component";
-import './collection-preview.styles.scss';
+import "./collection-preview.styles.scss";
 
-const CollectionPreview = ({title, items}) => (
-    <div className="collection-preview">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <div className = 'preview'>
-            {items
-            //Filter to only display the first 4 items
-                .filter((item, index) => index < 4)
-                .map(({id, ...otherItemProps}) =>(
-                    <CollectionItem key={id} {...otherItemProps}/>
-                ))}
-        </div>
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        //Filter to only display the first 4 items
+        .filter((item, index) => index < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
     </div>
+  </div>
 );
 
 export default CollectionPreview;
