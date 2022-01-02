c91caebbc799c0f2873b729ab9ac6fab53697eb2
Jack <60209373+OpoJack@users.noreply.github.com>
2022-01-01 23:42:57 -0500

    Added react-router-dom
    Also added some routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Routes} from 'react-router-dom';
+
+//Change to knife page later
+const HatsPage = () => (
+  <div className='hats'>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/hats' element={<HatsPage/>} />
+      </Routes>
     </div>
   );
 }
