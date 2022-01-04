4577566978ea578b0a1a86093f62e2ce6bf39987
Jack <60209373+OpoJack@users.noreply.github.com>
2022-01-03 22:12:48 -0500

    Added Firebase Google sign-in
    Started Firebase (Firestore) database.
    Added listeners in App.js to determine if user is already logged in.

--- src/App.js
+++ src/App.js
@@ -5,17 +5,45 @@ import { Route, Routes} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Routes>
-        <Route path='/' element={<HomePage/>} />
-        <Route path='/shop/' element={<ShopPage/>}/>
-      </Routes>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  //Sets the state of the current user
+  //Keeps user data in the local storage of the browser, persistant login
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  //Clean up the listener when the component unmounts
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser}/>
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path='/shop/' element={<ShopPage/>}/>
+          <Route path='/signin' element={<SignInAndSignUpPage/>}/>
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./header.styles.scss";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { auth } from "../../firebase/firebase.utils";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -16,6 +17,19 @@ const Header = () => (
       <Link className="option" to="/shop">
         CONTACT
       </Link>
+      {
+        //If user is logged in, render the Sign Out button
+        //else, render the Sign In button
+        currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signin">
+            SIGN IN
+          </Link>
+        )
+      }
     </div>
   </div>
 );
