97a63ec4b88e85dcdc21d1a29d0b4a1ba117209e
Jack <60209373+OpoJack@users.noreply.github.com>
2022-02-07 15:06:20 -0500

    Created cartItem component
    Also connected cart dropdown with cartItems reducer

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -3,6 +3,7 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
 
+  //If the itemid is already in the cart, increase the quantity of that item
   if (existingCartItem) {
     return cartItems.map(cartItem =>
       cartItem.id === cartItemToAdd.id
@@ -11,5 +12,6 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     )
   }
 
+  //Increment the quantity of the item if it is not in the cart
   return [...cartItems, {...cartItemToAdd, quantity: 1}];
 };
