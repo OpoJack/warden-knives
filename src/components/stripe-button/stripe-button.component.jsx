1509cf2bacff64c6ccc13d100494cab99a10f3af
Jack <60209373+OpoJack@users.noreply.github.com>
2022-02-23 12:22:27 -0500

    Add react-stripe-checkout
    Created StripeCheckout component with all props necessary.

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51KWMoUHgJAt7xTWJVSlQWdnpZY1UeblrSiatSTZs1eYyqkp7UB9KMyaZ7V2otO66Bmzeq765qy2rmSfzZOZ2Cm2q004IJzN1XI";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Check out"
+      name="Warden Knives"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Check out"
+      token={(token) => console.log(token)}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
